c589e1d75a28b799e01fcd7048720201ff7a19d6
julsCadenas <jscadenas12@gmail.com>
2024-06-28 11:31:42 +0800

    added my resume

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,6 +1,6 @@
 import myphoto from '../../src/images/myphoto.png';
 import React, { useEffect, useState } from 'react';
-
+import resume from '../media/Julian-Sebastian-Cadenas-Resume.pdf';
 
 const About = () => {
     const [displayText, setDisplayText] = useState('');
@@ -25,11 +25,7 @@ const About = () => {
 
     const handleNameClick = () => {
         startAnimation();
-    };
-
-    const click2 = () => {
-        alert('My resume is currently unavailable');
-    }                                                                                                                               
+    };                                                                                                                          
 
     const bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     const bio2 = "Currently pursuing a Bachelor's degree in Computer Engineering at Far Eastern University - Institute of Technology, I am a driven individual with a passion for software development. As a Scholar, I maintain a specific GPA every trimester, showcasing my dedication to academic excellence."
@@ -41,7 +37,9 @@ const About = () => {
                 <div className='myname' onClick={startAnimation}>{displayText || originalText}</div>
                 <div className='course'>BS Computer Engineering</div>
                 <div className='bio'>{bio2}</div>
-                <button class="resumeBtn" onClick={() => click2()}>RESUME</button>
+                <a href={resume} download='Julian-Sebastian-Cadenas-Resume.pdf'>
+                    <button class="resumeBtn">RESUME</button>
+                </a>
             </div>
             <div className='imagecontainer'>
                 <img src={myphoto}/>
